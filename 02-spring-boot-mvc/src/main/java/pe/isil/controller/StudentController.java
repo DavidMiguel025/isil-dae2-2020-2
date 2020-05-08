bdb0f2eaab011d5aeb51d0377784a9dca99c8e33
Jose Ventura <ventura.jos3@gmail.com>
2020-05-07 20:48:15 -0500

    add student save

--- 02-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
+++ 02-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
@@ -3,6 +3,7 @@ package pe.isil.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pe.isil.model.Student;
 
 import java.time.LocalDate;
@@ -14,16 +15,15 @@ import java.util.List;
 @Controller
 public class StudentController {
 
+    List<Student> students = new ArrayList<>(
+            Arrays.asList(
+                    new Student("42897534", "Jose", "Ventura", "Arteaga", LocalDate.of(1992, 3, 31)),
+                    new Student("32597531", "Franco", "Ventura", "Arteaga", LocalDate.of(1996, 2, 12))
+            )
+    );
 
     @GetMapping("/students")
     public String getStudentList(Model model){
-
-        List<Student> students = new ArrayList<>(
-                Arrays.asList(
-                    new Student("42897534", "Jose", "Ventura", "Arteaga", LocalDate.of(1992, 3, 31)),
-                    new Student("32597531", "Franco", "Ventura", "Arteaga", LocalDate.of(1996, 2, 12))
-                )
-        );
 
         model.addAttribute("students",students);
         return "student";
@@ -33,6 +33,18 @@ public class StudentController {
     public String addStudent(Model model){
         model.addAttribute("student", new Student());
         return "student-add";
+    }
+
+    @PostMapping("/students/save")
+    public String saveStudent(Model model, Student student){
+
+        //save
+        students.add(student);
+
+        //list
+        model.addAttribute("students",students);
+
+        return "student";
     }
 
 }
