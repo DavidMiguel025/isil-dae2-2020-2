38d171464786cfaf5415d5afe5db45cdce972bf0
Jose Ventura <ventura.jos3@gmail.com>
2020-05-07 21:54:16 -0500

    student edit

--- 02-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
+++ 02-spring-boot-mvc/src/main/java/pe/isil/controller/StudentController.java
@@ -1,30 +1,29 @@
 package pe.isil.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pe.isil.model.Student;
+import pe.isil.service.StudentService;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
 @Controller
 public class StudentController {
 
-    List<Student> students = new ArrayList<>(
-            Arrays.asList(
-                    new Student("42897534", "Jose", "Ventura", "Arteaga", LocalDate.of(1992, 3, 31)),
-                    new Student("32597531", "Franco", "Ventura", "Arteaga", LocalDate.of(1996, 2, 12))
-            )
-    );
+    final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping("/students")
     public String getStudentList(Model model){
-
+        List<Student> students = studentService.getAll();
         model.addAttribute("students",students);
         return "student";
     }
@@ -37,14 +36,16 @@ public class StudentController {
 
     @PostMapping("/students/save")
     public String saveStudent(Model model, Student student){
-
         //save
-        students.add(student);
+        studentService.save(student);
+        return "redirect:/students";
+    }
 
-        //list
-        model.addAttribute("students",students);
-
-        return "student";
+    @GetMapping("/students/edit/{documentNumber}")
+    public String studentForUpdate(@PathVariable String documentNumber, Model model){
+        Student currentStudent = studentService.findById(documentNumber);
+        model.addAttribute("student", currentStudent);
+        return "student-edit";
     }
 
 }
