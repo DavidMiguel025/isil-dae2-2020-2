26514cdd1d153ec9d5f89c22b7eb985ee2205332
Jose Ventura <ventura.jos3@gmail.com>
2020-05-14 22:01:20 -0500

    add 03 spring boot mvc jdbc

---
+++ 03-spring-boot-mvc-jdbc/src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+
+insert into product values (1, 'madera de cedro', 15.5);
+insert into product values (2, 'clavos glavanizados', 6.0);
---
+++ 03-spring-boot-mvc-jdbc/src/main/resources/schema-h2.sql
@@ -0,0 +1,6 @@
+
+create table product (
+    id identity primary key,
+    name varchar(100) not null,
+    price numeric not null
+);
